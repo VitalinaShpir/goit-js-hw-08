d3dd56be31c23a3dc9c6220c0a5633d6eb351b8c
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-12 17:04:58 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+
+const listItem = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+  )
+  .join("");
+
+  document.querySelector(".gallery").insertAdjacentHTML("beforeend", listItem);
+
+  const lightbox = new SimpleLightbox(".gallery__link", {captionsData: 'alt', captionDelay: 250 });
