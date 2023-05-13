e2c3f3d913fa52dda97620b70c1172caacf563ad
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-14 01:06:00 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,21 @@
 
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+        const player = new Player(iframe);
+    
+        player.on('timeupdate', function() {
+            console.log('played the video!');
+            if (localStorage.getItem('iframe', 'timeupdate')) {
+                iframe.currentTime = parseFloat(localStorage.getItem('iframe', 'timeupdate'));
+              }
+            localStorage.setItem('timeupdate', iframe.currentTime);
+        });
+    
+        player.getVideoTitle().then(function(title) {
+            console.log('title:', title);
+        });
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+
