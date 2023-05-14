c645b0dac6115dc62371e925e471573ab65bb056
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-14 20:40:05 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,13 +6,14 @@ const iframe = document.querySelector('iframe');
         const player = new Player(iframe);
     
         player.on('timeupdate', function() {
-            console.log('played the video!');
-            if (localStorage.getItem('iframe', 'timeupdate')) {
-                iframe.currentTime = parseFloat(localStorage.getItem('iframe', 'timeupdate'));
-              }
-            localStorage.setItem('timeupdate', iframe.currentTime);
+            // console.log('played the video!');
+            // if (localStorage.getItem('videoplayer-current-time')) {
+            //     iframe.currentTime = parseFloat(localStorage.getItem('videoplayer-current-time'));
+            //   }
+            localStorage.setItem('videoplayer-current-time', iframe.currentTime);
+            console.log('Hello');
         });
-    
+        
         player.getVideoTitle().then(function(title) {
             console.log('title:', title);
         });
