0e2085c41632f87a89483bd8f5d4720e8ccadc62
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-15 18:23:06 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,9 @@
+import '../css/common.css';
 import Player from '@vimeo/player';
-import { throttle } from 'lodash.throttle';
-
+import { throttle } from 'lodash';
+const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const TIME_KEY = 'videoplayer-current-time';
-
 const onPlay = function (data) {
   const strigifyData = JSON.stringify(data);
   localStorage.setItem(TIME_KEY, strigifyData);
@@ -30,5 +29,3 @@ function resumePlayback() {
   }
 }
 resumePlayback();
-
-console.log('Hello');
