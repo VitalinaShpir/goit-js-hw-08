48016769dbd3b872f0693127b1f445c5a885346d
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-15 23:38:25 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,28 +4,21 @@ import { throttle } from 'lodash';
 const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, strigifyData);
+const onPlay = function ({seconds}) {
+ 
+  localStorage.setItem(TIME_KEY, seconds);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+  const paused = localStorage.getItem(TIME_KEY);
+  if (paused === null) {
     return;
   }
-  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
-  if (paused) {
+
+  
     player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
-  }
+      .setCurrentTime(paused);
+      
+  
 }
 resumePlayback();
