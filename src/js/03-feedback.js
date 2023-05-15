19b806a9ffcd897a13042a7d4a7672e4bc4fd804
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-15 21:21:40 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import '../css/common.css';
 
+import { throttle } from 'lodash';
+
+const refs = {
+  form: document.querySelector(".feedback-form"),
+  input: document.querySelector(".feedback-form input"),
+  textarea: document.querySelector(".feedback-form textarea")
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', onEmailInput);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt){
+
+};
+
+
+function onEmailInput(evt){
+
+};
+
+function onTextareaInput(evt){
+const value = evt.currentTarget.value;
+
+console.log(value)
+};
